b6a0053734451eb7707c5cb3efd41032fc614bbe
mohamad <130075161+mohamadzeza@users.noreply.github.com>
2023-12-17 09:58:59 -0800

    end project

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+const red = document.getElementById('red')
+const blue = document.getElementById('blue')
+const green = document.getElementById('green')
+const yellow = document.getElementById('yellow')
+const black = document.getElementById('black')
+const body = document.getElementById('body')
+function red2(){
+    body.style.backgroundColor = "red"
+    body.style.color = "white"
+}
+function blue2(){
+    body.style.backgroundColor = "blue"
+    body.style.color = "white"
+}
+function green2(){
+    body.style.backgroundColor = "green"
+    body.style.color = "white"
+}
+function yellow2(){
+    body.style.backgroundColor = "yellow"
+    body.style.color = "white"
+}
+function black2(){
+    body.style.backgroundColor = "black"
+    body.style.color = "white"
+}
