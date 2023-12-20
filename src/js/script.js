21df000a76672b12bad231fa1b2d1690cbe8c306
amir <amir@gmail.com>
2023-12-20 18:37:42 +0330

    add code for function

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,10 @@
-const red = document.getElementById('red')
-const blue = document.getElementById('blue')
-const green = document.getElementById('green')
-const yellow = document.getElementById('yellow')
-const black = document.getElementById('black')
-const body = document.getElementById('body')
-function red2(){
-    body.style.backgroundColor = "red"
-    body.style.color = "white"
-}
-function blue2(){
-    body.style.backgroundColor = "blue"
-    body.style.color = "white"
-}
-function green2(){
-    body.style.backgroundColor = "green"
-    body.style.color = "white"
-}
-function yellow2(){
-    body.style.backgroundColor = "yellow"
-    body.style.color = "white"
-}
-function black2(){
-    body.style.backgroundColor = "black"
-    body.style.color = "white"
+const ul = document.querySelector('ul')
+function changeColor(color){
+    body.style.backgroundColor = color;
+    if(body.style.backgroundColor === "black"){
+        ul.style.color = "white"
+    }
+    else{
+        ul.style.color = "black"
+    }
 }
